64df25acdb5317e7f46519337592a34d38082273
Geraint Palmer <gaz@twothirdsdesign.co.uk>
2015-02-24 21:51:15 +0000

    frame out plugin, add basic loader structure, add phpflickr submodule

---
+++ class.jagp.php
@@ -0,0 +1,98 @@
+<?php 
+
+/**
+* 
+*/
+class Jagp
+{
+	/**
+	 * Holds the singleton instance of this class
+	 * @since 2.3.3
+	 * @var Jagp
+	 */
+	static $instance = false;
+
+	/**
+	 * Singleton
+	 * @static
+	 */
+	public static function init()
+	{
+		if ( ! self::$instance ) {
+			if ( did_action( 'plugins_loaded' ) )
+				self::plugin_textdomain();
+			else
+				add_action( 'plugins_loaded', array( __CLASS__, 'plugin_textdomain' ), 99 );
+
+			self::$instance = new Jagp;
+
+			self::$instance->plugin_upgrade();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Constructor.  Initializes WordPress hooks
+	 *
+	 * @return void
+	 * @author Geraint Palmer
+	 **/
+	private function Jagp()
+	{
+		# code...
+	}
+
+	/**
+	 * undocumented function
+	 *
+	 * @return void
+	 * @author 
+	 **/
+	private function plugin_upgrade()
+	{
+		# code...
+	}
+
+	/**
+	 * Load language files
+	 *
+	 * @return void
+	 * @author 
+	 **/
+	public static function plugin_textdomain()
+	{
+		load_plugin_textdomain( 'jagp', false, dirname( plugin_basename( JAGP__PLUGIN_FILE ) ) . '/languages/' );
+	}
+
+	/**
+	 * Attached to activate_{ plugin_basename( __FILES__ ) } by register_activation_hook()
+	 * @static
+	 * @return void
+	 */
+	public static function plugin_activation()
+	{
+	}
+
+	/**
+	 * Removes all connection options
+	 *
+	 * @return void
+	 * @author 
+	 **/
+	public static function plugin_deactivation()
+	{
+	}
+
+	/**
+	 * undocumented function
+	 *
+	 * @return void
+	 * @author 
+	 **/
+	public static function load_modules()
+	{
+	}
+}
+
+?>
--- jagp.php
+++ jagp.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Just another Gallery Plugin by @geraintp
  * Plugin URI: http://geraint.co/plugins/jagp/
- * Description: Bring the power of the WordPress.com cloud to your self-hosted WordPress. Jetpack enables you to connect your blog to a WordPress.com account to use the powerful features normally only available to WordPress.com users.
+ * Description: COMING SOON!
  * Author: Geraint Palmer
  * Version: 0.0.1
  * Author URI: http://geraint.co
@@ -16,7 +16,14 @@ define( 'JAGP__VERSION',            '0.0.1' );
 define( 'JAGP__PLUGIN_DIR',         plugin_dir_path( __FILE__ ) );
 define( 'JAGP__PLUGIN_FILE',        __FILE__ );
 
+require_once( JAGP__PLUGIN_DIR . 'class.jagp.php' );
+
+if ( is_admin() ) {
+	//require_once( JAGP__PLUGIN_DIR . 'class.jagp-admin.php'     );
+}
+
 register_activation_hook( __FILE__, array( 'Jagp', 'plugin_activation' ) );
 register_deactivation_hook( __FILE__, array( 'Jagp', 'plugin_deactivation' ) );
 
-require_once( JETPACK__PLUGIN_DIR . 'class.jagp.php' );
+add_action( 'init', array( 'Jagp', 'init' ) );
+add_action( 'plugins_loaded', array( 'Jagp', 'load_modules' ), 100 );
